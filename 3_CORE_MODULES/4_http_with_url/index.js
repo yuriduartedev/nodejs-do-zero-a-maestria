dd3f68815d2021bf4b67017c578cfeb45967d9f5
Yuri Duarte <yuriduartedev@gmail.com>
2022-10-12 19:56:19 -0400

    http with url modules

---
+++ 3_CORE_MODULES/4_http_with_url/index.js
@@ -0,0 +1,27 @@
+const http = require("http");
+const url = require("url");
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  const urlInfo = url.parse(req.url, true);
+  const name = urlInfo.query.name;
+
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+
+  if (!name) {
+    res.end(
+      `
+        <h1>Fill your name.</h1>
+        <form method="GET"><input type="text" name="name"/> <input type="submit" value="Enviar" /></form>
+      `
+    );
+  } else {
+    res.end(`<h1>Welcome ${name}</h1>`);
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
