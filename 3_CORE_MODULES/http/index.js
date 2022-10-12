2b33f272a08664b671ac6016a58e46467a4c5fc4
Yuri Duarte <yuriduartedev@gmail.com>
2022-10-12 19:40:34 -0400

    http

---
+++ 3_CORE_MODULES/http/index.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.write("Hello HTTP");
+  res.end();
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
